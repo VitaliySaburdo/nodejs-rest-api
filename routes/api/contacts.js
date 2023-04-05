77543248944429c0531a65d1d66315a67248fdd1
VitaliySaburdo <vsaburdo@gmail.com>
2023-04-05 15:59:51 +0300

    finish id

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,15 +2,31 @@ const express = require("express");
 
 const contacts = require("../../models/contacts");
 
+const { HttpError } = require("../../helpers");
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  try {
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({ message });
+  }
 });
 
 router.post("/", async (req, res, next) => {

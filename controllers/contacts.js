6a00f04c60cc3099fa8c5c2b598fa6f43a4f68f1
VitaliySaburdo <vsaburdo@gmail.com>
2023-04-09 17:37:03 +0300

    decoration add

---
+++ controllers/contacts.js
@@ -0,0 +1,48 @@
+const contacts = require("../models/contacts");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const getAllContacts = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getContactsById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+
+const createContact = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
+const changeContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactsById: ctrlWrapper(getContactsById),
+  createContact: ctrlWrapper(createContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  changeContactById: ctrlWrapper(changeContactById),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,83 +1,25 @@
 const express = require("express");
-const Joi = require("joi");
-
-const contacts = require("../../models/contacts");
-
-const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
-const addShema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
+const ctrl = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+const { validateBody } = require("../../middlewares");
 
-router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  try {
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404, "Not Found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+const schemas = require("../../schemas/contacts");
 
-router.post("/", async (req, res, next) => {
-  console.log(req.body);
-  try {
-    const { error } = addShema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing required name field");
-    }
+router.get("/", ctrl.getAllContacts);
 
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:contactId", ctrl.getContactsById);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, "Not Found");
-    }
-    res.status(200).json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validateBody(schemas.addShema), ctrl.createContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { error } = addShema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "missing fields");
-    }
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, "Not Found");
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrl.deleteContact);
+
+router.put(
+  "/:contactId",
+  validateBody(schemas.addShema),
+  ctrl.changeContactById
+);
 
 module.exports = router;

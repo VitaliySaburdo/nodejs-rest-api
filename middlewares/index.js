f381234885934eeb43e635962d631dbeeeba9c6d
VitaliySaburdo <vsaburdo@gmail.com>
2023-04-18 20:42:10 +0300

    authenticate

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validateBody = require("./validation");
 const validateCreateBody = require("./validationCreateBody");
 const isValidId = require("./isValidId");
 const validatePatchBody = require("./validationPatchBoby");
+const authenticate = require("./authenticate");
 
 module.exports = {
   validateBody,
   validateCreateBody,
   isValidId,
   validatePatchBody,
+  authenticate,
 };

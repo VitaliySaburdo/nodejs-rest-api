859d952c69b73f6a2776066a5901a6f372fc8c7c
VitaliySaburdo <vsaburdo@gmail.com>
2023-04-22 08:41:58 +0300

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,14 +27,14 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
+  const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw HttpError(401);
+    throw HttpError(401, "Email or password is wrong");
   }
   const payload = {
     id: user._id,

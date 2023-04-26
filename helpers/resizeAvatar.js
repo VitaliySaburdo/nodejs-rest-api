fafcff300938cc2ff56cdd4a0a43d5b04d0d07e9
VitaliySaburdo <vsaburdo@gmail.com>
2023-04-26 20:22:11 +0300

    try

--- helpers/resizeAvatar.js
+++ helpers/resizeAvatar.js
@@ -1,9 +1,13 @@
 const Jimp = require("jimp");
 
 async function resize(img) {
-  const image = await Jimp.read(img);
-  await image.resize(100, 100);
-  await image.writeAsync(img);
+  try {
+    const image = await Jimp.read(img);
+    await image.resize(100, 100);
+    await image.writeAsync(img);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = resize;
